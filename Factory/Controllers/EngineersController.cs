adf23209150a334e2fc4de2a532ef76ac32b84a8
Phil Curran <pecurran@hotmail.com>
2022-03-26 14:50:54 -0700

    added views and controllers for Engineers and Machines.

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+
+namespace Factory.Controllers;
+
+public class EngineersController : Controller
+{
+    public IActionResult AddEngineer()
+    {
+        return View();
+    }
+    
+    public IActionResult AddMachine()
+    {
+        return View();
+    }
+    
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    public IActionResult Delete()
+    {
+        return View();
+    }
+    
+    public IActionResult Details()
+    {
+        return View();
+    }
+    
+    public IActionResult Edit()
+    {
+        return View();
+    }
+    
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
